4d6049b93759347108cd8f560b66539622b12be9
ngoctam033 <nguyenngoctam0332003@gmail.com>
2025-09-05 11:07:40 +0700

    add route

--- data_source/main.py
+++ data_source/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import load_user_api, load_order_api, load_warehouse
+from routers import load_user_api, load_order_api, load_warehouse, load_order_items_api  # Import router
 
 app = FastAPI()
 
@@ -7,6 +7,7 @@ app = FastAPI()
 app.include_router(load_user_api.router)
 app.include_router(load_order_api.router)
 app.include_router(load_warehouse.router)
+app.include_router(load_order_items_api.router)
 
 # uvicorn main:app --reload --host 0.0.0.0 --port 9000
 # 
